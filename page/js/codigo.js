35bc4c6ba08233a87883e829e2c4b419c9e0fb02
Noxtrom <42249958+Noxtrom@users.noreply.github.com>
2019-08-03 11:29:05 -0500

    Update codigo.js

--- page/js/codigo.js
+++ page/js/codigo.js
@@ -1,30 +1,84 @@
-var check_koch = document.getElementById('koch');
-var check_sierpinski = document.getElementById('sierpinski');
-var dibujo = document.getElementById('fractal');
-var lienzo = dibujo.getContext("2d");
-var txt_nivel = document.getElementById('nivel');
-var btn_subir = document.getElementById('btn-arriba');
-var btn_bajar = document.getElementById('btn-abajo');
-var btn_dibujar = document.getElementById('btn-dibujar');
+// This is the script and there's a mini vocabulary
+// Esto es el codigo y aqui ahi un pequeño vocabulario
+
+// +--------------+-------------+
+// |   ESPAÑOL    |   ENGLISH   |
+// +--------------+-------------+
+// |   nivel      |   level     |
+// |   aumentar   |   increase  |
+// |   aumentar   |   decrease  |
+// |   dibujar    |   draw      |
+// |   dibujo     |   picture   |
+// |   lienzo     |   canvas    |
+// +--------------+-------------+
+
+// ELEMENTS
+// ELEMENTOS
+
+// Global variables
+// Variables globales
+
 var ancho = dibujo.width;
 var alto = dibujo.height;
-var color = "red";
 var x_0_inicial = 50;
 var x_n_inicial = ancho-50;
 var y_0_inicial = ancho/2;
 var y_n_inicial = ancho/2;
+var color = "red";
+
+// Checkboxes
+// Casillas de verificacion
+var check_koch = document.getElementById('koch');
+var check_sierpinski = document.getElementById('sierpinski');
+
+// Textboxes
+// Cajas de textos
+var txt_nivel = document.getElementById('nivel');
+
+// Butttons
+// Botones
+var btn_aumentar = document.getElementById('btn-arriba');
+var btn_disminuir = document.getElementById('btn-abajo');
+var btn_dibujar = document.getElementById('btn-dibujar');
+
+// Auxiliary
+// Auxiliares
+var dibujo = document.getElementById('fractal');
+var lienzo = dibujo.getContext("2d");
+
+// Levels
+// ------
+// The code generates ( 4 ^ level + 1 ) points, that's why the browser may
+// crash if you use a higher value
+
+// Niveles
+// -------
+// El codigo genera ( 4 ^ nivel + 1 ) puntos, por eso no es recomendable
+// dibujar a un nivel muy alto pues puede hacer que el navegador crashee
+
+var nivel_max = 8;
+var nivel_min = 0;
+var nivel = txt_nivel.value;
+if (nivel.value==""){
+  nivel = nivel_min;
+}
+
+// EVENTS
+// EVENTOS
+
+// Buttons' events
+// Eventos de botones
 
 btn_dibujar.addEventListener('click', dibujar);
+btn_aumentar.addEventListener('click', aumentar);
+btn_disminuir.addEventListener('click', disminuir);
+txt_nivel.addEventListener('keyup', leer);
 
-function dibujar(){
-  if(check_koch.checked){
-    fractal_koch_0();
-  } else if(check_koch.checked){
-    fractal_sierpinski_0();
-  } else {
-    alert("Elija uno de los fractales porfavor ;3");
-  }
-}
+// FUNCTIONES
+// FUNCIONES
+
+// Base function
+// Funcion base
 
 function dibujarLinea(color,x_0,y_0,x_n,y_n){
   lienzo.beginPath();
@@ -35,13 +89,58 @@ function dibujarLinea(color,x_0,y_0,x_n,y_n){
   lienzo.closePath();
 }
 
+// Auxiliary functions
+// Funciones auxiliares
+
+function leer(){
+  nivel=txt_nivel.value;
+}
+
 function limpiarLienzo(){
   lienzo.clearRect(0,0,ancho,alto);
 }
 
+// Buttons' functions
+// Funciones de botones
+
+function aumentar(){
+  if(nivel<nivel_max){
+    nivel++;
+  }
+  txt_nivel.value=nivel;
+}
+
+function disminuir(){
+  if(nivel>nivel_min){
+    nivel--;
+  }
+  txt_nivel.value=nivel;
+}
+
+function dibujar(){
+  if(nivel<nivel_min || nivel>nivel_max){
+    nivel=nivel_min;
+    txt_nivel.value=nivel;
+  }
+  nivel=txt_nivel.value;
+  if(check_koch.checked){
+    fractal_koch_0();
+  } else if(check_koch.checked){
+    fractal_sierpinski_0();
+  } else {
+    alert("Elija uno de los fractales porfavor ;3");
+  }
+}
+
+// Recursivity functions
+// Funciones de recursividad
+
+// 1. Koch's Snowflake
+// 1. Estrella de Koch
+
 function fractal_koch_0(){
   limpiarLienzo();
-  fractal_koch(txt_nivel.value, color, x_0_inicial, y_0_inicial, x_n_inicial, y_n_inicial);
+  fractal_koch(nivel, color, x_0_inicial, y_0_inicial, x_n_inicial, y_n_inicial);
 }
 
 function fractal_koch(nivel,color,x_0, y_0, x_n, y_n) {
@@ -58,5 +157,19 @@ function fractal_koch(nivel,color,x_0, y_0, x_n, y_n) {
     fractal_koch(nivel-1,color,x_b,y_b,x_n,y_n);
   } else {
     dibujarLinea(color,x_0, y_0, x_n, y_n);
+  }
+}
+
+// 2. Sierpinski's Triangle
+// 2. Triangulo de Sierpinski
+
+function fractal_sierpinski_0(){
+  limpiarLienzo();
+  fractal_sierpinski(nivel, color, x_0_inicial, y_0_inicial, x_n_inicial, y_n_inicial);
+}
+
+function fractal_sierpinski(){
+  if(nivel>0){
+
   }
 }
