1196c7e03f51ead8862a55374d03bfe6ff521b25
Noxtrom <42249958+Noxtrom@users.noreply.github.com>
2019-08-04 20:30:21 -0500

    Update menu.js

--- bootstrap/js/menu.js
+++ bootstrap/js/menu.js
@@ -1,31 +1,32 @@
 var r_1 = document.getElementById('radio-1');
 var r_2 = document.getElementById('radio-2');
 var r_3 = document.getElementById('radio-3');
-var l_1 = document.getElementById('label-1');
-var l_2 = document.getElementById('label-2');
-var l_3 = document.getElementById('label-3');
 var radios = [r_1, r_2, r_3];
-var labels = [l_1, l_2, l_3];
+var menu = [r_1.parentElement, r_2.parentElement, r_3.parentElement];
 
-r_1.checked="true";
+r_1.checked = "true";
 
-for(var i=0;0<labels.length;i++){
-  _switch(i);
-}
+var num=0;
 
-function _switch(i) {
-  labels[i].addEventListener('click', function(){
-    if(!radios[i].checked){
-      if(radios[i].checked){
-        labels.splice(i,1);
+for(var i=0;i<menu.length;i++){
+  menu[i].addEventListener('click', function comprobar() {
+    num++;
+    console.log("Veces ejecutado: " + num);
+    // TODO Idk why this runs twice ;-;
+    // TODO No se porque se ejecuta 2 veces ;-;
+    for (var i = 0; i < menu.length; i++) {
+      console.log("Comprobando menu " + (i + 1));
+      if (menu[i].children[1].checked) {
+        menu[i].classList.add("active");
+        menu[i].children[0].setAttribute("src", "img/" + (i + 1) + ".png");
+        console.log("Se selecciono el menu " + (i + 1));
+      } else {
+        menu[i].classList.remove("active");
+        menu[i].children[0].setAttribute("src", "img/" + (10 * (i + 1)) + ".png");
+        console.log("Se deselecciono el menu " + (i + 1))
       }
-      for(var j=0; j<labels.length; j++){
-        labels[j].classList.remove("active");
-      }
-      labels[i].classList.add("active");
-      labels.push(labels[i]);
-      labels.sort();
     }
-    labels[i].classList.add("active");
-  })
+  });
+  console.log("Evento "+(i+1)+" añadido");
 }
+
